46f668c36adcae6a8204be993dca6d77c86ff250
kowsikadevi21082006 <144236845+kowsikadevi21082006@users.noreply.github.com>
2023-12-20 17:34:02 +0530

    done

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
-const addButtons = document.getElementById();
-const Buttons = document.getElementById();
-const Recipe = document.getElementById();
+document.getElementById("button").addEventListener('click', () => {
+    // Use the specific meal name "Arrabiata" in the API URL
+    let mealName = document.getElementById('inputName').value 
+    let details = document.getElementById("details");
+    details.innerHTML = "";
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`)
+        .then(response => response.json())
+        .then(data => {
+            const items = document.getElementById("items");
+            items.innerHTML = "";
+
+            if (data.meals == null) {
+                document.getElementById("msg").style.display = "block";
+            } else {
+                document.getElementById("msg").style.display = "none";
+                data.meals.forEach(meal => {
+                    const itemDiv = document.createElement("div");
+                    itemDiv.className = "m-2 singleItem";
+                    itemDiv.setAttribute('onclick', `details('${meal.idMeal}')`);
+                    
+                    const itemInfo = `
+                        <div class="card" style="width: 12rem;">
+                            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                            <div class="card-body text-center">
+                                <h5 class="card-text1">${meal.strMeal}</h5>
+                            </div>
+                        </div>
+                    `;
+                    itemDiv.innerHTML = itemInfo;
+                    items.appendChild(itemDiv);
+                });
+            }
+        })
+        .catch(error => {
+            console.error(`Error: ${error.message}`);
+        });
+});
+
+function details(id) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => res.json())
+        .then(detail => {
+            let meal = detail.meals[0];
+            console.log(meal);
+
+            let details = document.getElementById("details");
+            details.innerHTML = "";
+
+            let detailsDiv = document.createElement("div");
+            let detailsInfo = `
+                <div class="card " style="width: 15rem;">
+                    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                    <div class="card-body ">
+                        <h3 class="card-text">${meal.strMeal}</h3>
+                        <h6>Ingredients</h6>
+                        <ul>
+                            <li>${meal.strArea}</li>
+                            <li>${meal.strCategory}</li>
+                            <li>${meal.strIngredient1}</li>
+                            <li>${meal.strIngredient2}</li>
+                            <li>${meal.strIngredient3}</li>
+                            <li>${meal.strIngredient4}</li>
+                        </ul>
+                    </div>
+                </div>
+            `;
+            detailsDiv.innerHTML = detailsInfo;
+            details.appendChild(detailsDiv);
+        })
+        .catch(error => {
+            console.error(`Error: ${error.message}`);
+        });
+}
